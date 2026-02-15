987e4d1974057665c64bce6fe658c6933168c56e
camiloperezII <camilomiguel.perezii@email.lcup.edu.ph>
2026-02-15 13:10:38 +0800

    Godot Game Folder
    This is the folder for the game I made in godot that is still work in progress

---
+++ final-project-(bscpe-3)---perez-ii,-camilo-miguel-g/scenes/main_character.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -700.0
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+var gravity =ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	#animation for walk/run 
+	if abs(velocity.x)> 1:
+		sprite_2d.animation = "TAKBO"
+	else:
+		sprite_2d.animation = "IDLE"
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		sprite_2d.animation = "TALON"
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("TALON") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("LEFT", "RIGHT")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 10)
+
+	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h =isLeft
